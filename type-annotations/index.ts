d19b31146f99e4ca18afd8913bfb118949466e21
nycbasic <samuel.poon@baruchmail.cuny.edu>
2022-12-19 11:45:08 -0500

    first commit

---
+++ type-annotations/index.ts
@@ -0,0 +1,11 @@
+let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+// movieTitle = 0;
+
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "Zero"
+
+let boolean: boolean = "true";
+boolean = false;
+// boolean = "false";

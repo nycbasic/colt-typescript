6d5d8e606a995bbf376d100ce1370e5beaa7ddfb
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-05 11:55:05 -0500

    adding type assertions, assertions with the DOM, working with events, building a todo list

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -1,2 +1,25 @@
 "use strict";
+// exclamation mark used for certain, a value will be returned that is not null.
 const btn = document.getElementById("btn");
+// Type Assertions to define the specific element
+const input = document.getElementById("todo-input");
+const form = document.getElementById("todo-form");
+const list = document.getElementById("todo-list");
+// For the event method, when using type script, needs to be set a type when not used with the event listner.
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitted!");
+    const newTodoText = input.value;
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    listItem.append(checkbox);
+    listItem.append(newTodoText);
+    list?.append(listItem);
+    input.value = "";
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", () => {
+//   alert(input.value);
+//   input.value = "";
+// });

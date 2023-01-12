b4a9bda9328d280bb32b8abb5cd30ef6cd6dbeef
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-12 16:07:15 -0500

    using interfaces with classes

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -1,40 +1,52 @@
 "use strict";
-class Player {
-    // class fields in TS is as is and does not need a type annotation
-    // private score = 0;
-    constructor(first, last, 
-    // private _score: number // private only stays within the class and does not extend or inherits
-    _score // protected passes to child classes with extends and pass inhertitance
-    ) {
-        this.first = first;
-        this.last = last;
-        this._score = _score;
-        this.secretMethod();
-    }
-    secretMethod() {
-        console.log("secret method!");
-    }
-    get fullName() {
-        return `${this.first} ${this.last}`;
-    }
-    get score() {
-        return this._score;
-    }
-    set score(newScore) {
-        if (newScore < 0) {
-            throw new Error("Score cannot be negative");
-        }
-        this._score = newScore;
+class Bike {
+    constructor(color) {
+        this.color = color;
     }
 }
-class SuperPlayer extends Player {
-    constructor() {
-        super(...arguments);
-        this.isAdmin = true;
+class Jacket {
+    constructor(brand, color) {
+        this.brand = brand;
+        this.color = color;
     }
-    maxScore() {
-        this._score = 999999;
+    print() {
+        console.log(`${this.color}, ${this.brand}`);
     }
 }
-const elton = new Player("Elton", "Steele", 4);
-elton.secretMethod();
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Prada", "black");
+// class Player {
+//   // class fields in TS is as is and does not need a type annotation
+//   // private score = 0;
+//   constructor(
+//     public first: string,
+//     public last: string,
+//     // private _score: number // private only stays within the class and does not extend or inherits
+//     protected _score: number // protected passes to child classes with extends and pass inhertitance
+//   ) {
+//     this.secretMethod();
+//   }
+//   private secretMethod(): void {
+//     console.log("secret method!");
+//   }
+//   get fullName() {
+//     return `${this.first} ${this.last}`;
+//   }
+//   get score(): number {
+//     return this._score;
+//   }
+//   set score(newScore: number) {
+//     if(newScore < 0) {
+//       throw new Error("Score cannot be negative")
+//     }
+//     this._score = newScore;
+//   }
+// }
+// class SuperPlayer extends Player {
+//   public isAdmin: boolean = true;
+//   maxScore() {
+//     this._score = 999999
+//   }
+// }
+// const elton = new Player("Elton", "Steele", 4);
+// elton.secretMethod();
--- classes/index.ts
+++ classes/index.ts
@@ -1,42 +1,64 @@
-class Player {
-  // class fields in TS is as is and does not need a type annotation
-  // private score = 0;
-  constructor(
-    public first: string,
-    public last: string,
-    // private _score: number // private only stays within the class and does not extend or inherits
-    protected _score: number // protected passes to child classes with extends and pass inhertitance
-  ) {
-    this.secretMethod();
-  }
+interface Colorful {
+  color: string;
+}
 
-  private secretMethod(): void {
-    console.log("secret method!");
-  }
+interface Printable {
+  print(): void;
+}
 
-  get fullName() {
-    return `${this.first} ${this.last}`;
-  }
+class Bike implements Colorful {
+  constructor(public color: string) {}
+}
 
-  get score(): number {
-    return this._score;
-  }
-
-  set score(newScore: number) {
-    if(newScore < 0) {
-      throw new Error("Score cannot be negative")
-    }
-    this._score = newScore;
+class Jacket implements Colorful, Printable {
+  constructor(public brand: string, public color: string) {}
+  print(): void {
+    console.log(`${this.color}, ${this.brand}`)
   }
 }
 
-class SuperPlayer extends Player {
-  public isAdmin: boolean = true;
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Prada", "black");
 
-  maxScore() {
-    this._score = 999999
-  }
-}
+// class Player {
+//   // class fields in TS is as is and does not need a type annotation
+//   // private score = 0;
+//   constructor(
+//     public first: string,
+//     public last: string,
+//     // private _score: number // private only stays within the class and does not extend or inherits
+//     protected _score: number // protected passes to child classes with extends and pass inhertitance
+//   ) {
+//     this.secretMethod();
+//   }
 
-const elton = new Player("Elton", "Steele", 4);
-elton.secretMethod();
+//   private secretMethod(): void {
+//     console.log("secret method!");
+//   }
+
+//   get fullName() {
+//     return `${this.first} ${this.last}`;
+//   }
+
+//   get score(): number {
+//     return this._score;
+//   }
+
+//   set score(newScore: number) {
+//     if(newScore < 0) {
+//       throw new Error("Score cannot be negative")
+//     }
+//     this._score = newScore;
+//   }
+// }
+
+// class SuperPlayer extends Player {
+//   public isAdmin: boolean = true;
+
+//   maxScore() {
+//     this._score = 999999
+//   }
+// }
+
+// const elton = new Player("Elton", "Steele", 4);
+// elton.secretMethod();

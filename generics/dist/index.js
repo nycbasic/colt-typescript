cd233013f044804240b97a48e856d9038d5a9594
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-13 15:23:52 -0500

    introduction to generics

---
+++ generics/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// type annotation
+const nums = [];
+// Generic
+const nums2 = [];
+// type annotation
+const colors = [];
+// Generic
+const colors2 = [];
---
+++ generics/src/index.ts
@@ -0,0 +1,9 @@
+// type annotation
+const nums: number[] = [];
+// Generic
+const nums2: Array<number> = [];
+
+// type annotation
+const colors: string[] = [];
+// Generic
+const colors2: Array<string> = [];

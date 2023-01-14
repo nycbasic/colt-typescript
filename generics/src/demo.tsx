2f7b147f1f993740108d0c8dd5f31701446a873b
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-13 19:39:49 -0500

    generics with arrow functions and tsx files

---
+++ generics/src/demo.tsx
@@ -0,0 +1,6 @@
+// const getRandomElement = <T,>(list: T[]): T => {
+//     const randIdx = Math.floor(Math.random() * list.length);
+//     return list[randIdx];
+// } 
+
+// when using tsx files for react, for generic arrow functions, the comma is use to keep the auto-complete as a closing element tag from confusing TS generics.

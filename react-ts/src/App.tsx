c3c68692f848b47ec8f3b2d4b6ec4950c590faad
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-18 12:04:51 -0500

    creating a shopping list component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
-import Greeter from './components/Greeter';
-
+import React from "react";
+import "./App.css";
+import ShoppingList from "./components/ShoppingList";
+const items = [
+  {
+    id: 1,
+    product: "Lemon",
+    quantity: 3,
+  },
+  {
+    id: 1,
+    product: "Chicken Breast",
+    quantity: 2,
+  }
+];
 function App() {
   return (
     <div className="App">
-     <Greeter person="Spoon"/>
+      <ShoppingList items={items} />
     </div>
   );
 }
